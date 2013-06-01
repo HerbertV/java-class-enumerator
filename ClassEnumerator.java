2f87bae488d9aa1142e5ce1c06fe20554a562033
Herbert Veitengruber <tohklidan@hotmail.com>
2013-06-01 12:44:54 +0200

    fixed Warnings: remove unused import and closing jar file now. And a little cleanup for better legibility.

--- ClassEnumerator.java
+++ ClassEnumerator.java
@@ -2,7 +2,6 @@ package pro.ddopson;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
@@ -10,12 +9,22 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 
-public class ClassEnumerator {
-	private static void log(String msg) {
+public class ClassEnumerator 
+{
+	
+	private static void log(String msg) 
+	{
 		//System.out.println("ClassDiscovery: " + msg);	
 	}
-
-	private static Class<?> loadClass(String className) {
+	
+	/**
+	 * loadClass
+	 * 
+	 * @param className
+	 * @return
+	 */
+	private static Class<?> loadClass(String className) 
+	{
 		try {
 			return Class.forName(className);
 		} 
@@ -24,7 +33,19 @@ public class ClassEnumerator {
 		}
 	}
 
-	private static void processDirectory(File directory, String pkgname, ArrayList<Class<?>> classes) {
+	/**
+	 * processDirectory
+	 * 
+	 * @param directory
+	 * @param pkgname
+	 * @param classes
+	 */
+	private static void processDirectory(
+			File directory, 
+			String pkgname, 
+			ArrayList<Class<?>> classes
+		) 
+	{
 		log("Reading Directory '" + directory + "'");
 		// Get the list of the files contained in the package
 		String[] files = directory.list();
@@ -47,33 +68,72 @@ public class ClassEnumerator {
 		}
 	}
 
-	private static void processJarfile(URL resource, String pkgname, ArrayList<Class<?>> classes) {
+	/**
+	 * processJarfile
+	 * 
+	 * @param resource
+	 * @param pkgname
+	 * @param classes
+	 */
+	private static void processJarfile(
+			URL resource, 
+			String pkgname, 
+			ArrayList<Class<?>> classes
+		)
+	{
 		String relPath = pkgname.replace('.', '/');
 		String resPath = resource.getPath();
 		String jarPath = resPath.replaceFirst("[.]jar[!].*", ".jar").replaceFirst("file:", "");
 		log("Reading JAR file: '" + jarPath + "'");
-		JarFile jarFile;
-		try {
+		JarFile jarFile = null;
+		try 
+		{
 			jarFile = new JarFile(jarPath);         
+			Enumeration<JarEntry> entries = jarFile.entries();
+			
+			while( entries.hasMoreElements() )
+			{
+				JarEntry entry = entries.nextElement();
+				String entryName = entry.getName();
+				String className = null;
+				if( entryName.endsWith(".class") 
+						&& entryName.startsWith(relPath) 
+						&& entryName.length() > (relPath.length() + "/".length())
+					)
+				{
+					className = entryName.replace('/', '.').replace('\\', '.').replace(".class", "");
+				}
+				
+				log("JarEntry '" + entryName + "'  =>  class '" + className + "'");
+				if( className != null )
+				{
+					classes.add(loadClass(className));
+				}
+			}
+			
 		} catch (IOException e) {
 			throw new RuntimeException("Unexpected IOException reading JAR File '" + jarPath + "'", e);
-		}
-		Enumeration<JarEntry> entries = jarFile.entries();
-		while(entries.hasMoreElements()) {
-			JarEntry entry = entries.nextElement();
-			String entryName = entry.getName();
-			String className = null;
-			if(entryName.endsWith(".class") && entryName.startsWith(relPath) && entryName.length() > (relPath.length() + "/".length())) {
-				className = entryName.replace('/', '.').replace('\\', '.').replace(".class", "");
-			}
-			log("JarEntry '" + entryName + "'  =>  class '" + className + "'");
-			if (className != null) {
-				classes.add(loadClass(className));
+		} finally {
+			if( jarFile != null )
+			{
+				try 
+				{
+					jarFile.close();
+				} catch( IOException e ) {
+					throw new RuntimeException("Unexpected IOException reading JAR File '" + jarPath + "'", e);
+				}
 			}
 		}
 	}
 	
-	public static ArrayList<Class<?>> getClassesForPackage(Package pkg) {
+	/**
+	 * getClassesForPackage
+	 * 
+	 * @param pkg
+	 * @return
+	 */
+	public static ArrayList<Class<?>> getClassesForPackage(Package pkg) 
+	{
 		ArrayList<Class<?>> classes = new ArrayList<Class<?>>();
 		
 		String pkgname = pkg.getName();
@@ -81,13 +141,15 @@ public class ClassEnumerator {
 	
 		// Get a File object for the package
 		URL resource = ClassLoader.getSystemClassLoader().getResource(relPath);
-		if (resource == null) {
+		if( resource == null )
+		{
 			throw new RuntimeException("Unexpected problem: No resource for " + relPath);
 		}
 		log("Package: '" + pkgname + "' becomes Resource: '" + resource.toString() + "'");
 
 		resource.getPath();
-		if(resource.toString().startsWith("jar:")) {
+		if( resource.toString().startsWith("jar:") ) 
+		{
 			processJarfile(resource, pkgname, classes);
 		} else {
 			processDirectory(new File(resource.getPath()), pkgname, classes);
